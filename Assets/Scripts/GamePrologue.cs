3ce41b641ba58f022913569dfa2199a0bd567283
y2eun <kye6735@naver.com>
2024-05-17 01:26:19 +0900

    feat: text 타이핑 효과 추가

--- Assets/Scripts/GamePrologue.cs
+++ Assets/Scripts/GamePrologue.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GamePrologue : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text prologueText;
+    string dialogue;
+
     void Start()
+    {
+        // 프롤로그 - 1
+        dialogue = "이번주 핫이슈는 이번에 신장 개업한 ‘좋은 햄버거 맛있는 햄버거’ 가게입니다 !";
+        StartCoroutine(Typing(dialogue));
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Typing(string talk)
     {
-        
+        // text null값으로 설정
+        prologueText.text = null;
+        for(int i = 0; i < talk.Length; i++) 
+        {
+            prologueText.text += talk[i];
+            // 속도
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }

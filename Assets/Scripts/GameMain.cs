5616e81794ddb4c54dd32dbd3e2cf7b899d9a3e5
y2eun <kye6735@naver.com>
2024-06-02 23:41:24 +0900

    [Feat] : n초 후 주문 창 이미지 및 버튼 나타남

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -7,11 +7,15 @@ using UnityEngine.UI;
 public class GameMain : MonoBehaviour
 {
     // Image
-    public Image characterImage; // 캐릭터 이미지 변수
+    public Image characterImage; // 캐릭터 이미지
+    public Image orderImage; // 음식 주문 이미지
 
     // Text
     public Text orderText;  // UI Text 요소를 연결하기 위한 변수
     public Text levelText;  // 레벨을 표시할 UI Text 요소
+
+    // Button
+    public Button yesBtn;
 
     // 변수 선언
     private int currentOrder = 1; // 주문수
@@ -20,9 +24,16 @@ public class GameMain : MonoBehaviour
 
     void Start()
     {
+        characterImage.gameObject.SetActive(false); // 캐릭터 이미지 비활성화
+        orderImage.gameObject.SetActive(false); // 음식 주문 이미지 비활성화
+        yesBtn.gameObject.SetActive(false); // '네' 버튼 비활성화
+
         UpdateOrderText();
         UpdateLevelText();
-        StartCoroutine(ShowCharacterImageAfterDelay(5f));
+
+        StartCoroutine(ShowCharacterImageAfterDelay(2.5f));
+
+        yesBtn.onClick.AddListener(OnYesButtonClick);
     }
 
     void Update()
@@ -87,10 +98,18 @@ public class GameMain : MonoBehaviour
         if (characterImage != null)
         {
             characterImage.gameObject.SetActive(true); // 캐릭터 이미지를 활성화
+            orderImage.gameObject.SetActive(true); // 음식 주문 이미지 비활성화
+            yesBtn.gameObject.SetActive(true); // '네' 버튼 활성화
         }
         else
         {
             Debug.LogError("Character Image is not assigned!");
         }
+    }
+
+    // 주문 시 '네' 버튼 
+    void OnYesButtonClick()
+    {
+        SceneManager.LoadScene("MainGameScene");
     }
 }
